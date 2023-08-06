affe8f4ff7fb7e741fdadf205f1ea43850c59168
halfside-net <105025415+halfside-net@users.noreply.github.com>
2023-08-05 23:48:41 -0400

    fix home button z index

--- pages/index.page.tsx
+++ pages/index.page.tsx
@@ -14,7 +14,7 @@ export function PageHead() {
 
   return (
     <head>
-      <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+      <meta httpEquiv="Content-Type" content="text/html;charset=utf-8" />
       <meta name="viewport" content="width=device-width,initial-scale=1" />
 
       <title>
